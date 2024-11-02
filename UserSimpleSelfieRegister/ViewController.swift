3ce4610e373e9c195f8906b0c3515fead53ae07d
ParkerChen <jchen440@my.bcit.ca>
2024-11-02 12:07:04 -0700

    added Basic-UI

---
+++ UserSimpleSelfieRegister/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UserSimpleSelfieRegister
+//
+//  Created by ZHANG ZHE on 2024-11-01.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
