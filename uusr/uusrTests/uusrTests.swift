edc80528e0f3c33b2685b664dacc8d0b9829625d
ParkerChen <jchen440@my.bcit.ca>
2024-11-02 12:10:55 -0700

    added default ios project with default setting(UI)

---
+++ uusr/uusrTests/uusrTests.swift
@@ -0,0 +1,17 @@
+//
+//  uusrTests.swift
+//  uusrTests
+//
+//  Created by Jianming Chen on 2024-11-02.
+//
+
+import Testing
+@testable import uusr
+
+struct uusrTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
